126c71c2ab74ee09ba3584607e73af88d2163a35
Szász Tibor <tszasz@gmail.com>
2017-09-21 14:19:41 +0200

    refactor(spiral): spiral is wrapped to function

--- spiral.py
+++ spiral.py
@@ -6,20 +6,20 @@ root = Tk()
 canvas = Canvas(root, width='400', height='400', bd=0, highlightthickness=0)
 canvas.pack()
 
-r = 100
 
-def create_point(angle,offsetX,offsetY):
+def create_point(r, angle, offsetX, offsetY):
     x = offsetX + r * cos(angle)
     y = offsetY + r * sin(angle)
-    canvas.create_oval(x, y, x+10, y+10, fill='red', width=0)
+    canvas.create_oval(x, y, x+10, y+10, fill='black', width=0)
 
-angle = 0
-n = 0
-c = 8.6
+def create_spiral(centerX, centerY):
+    c = 5
+    r = 100
+    for i in range(500):
+        angle = i * 137.5 * 180/pi
+        r = c * i**0.5
+        create_point(r, angle, centerX, centerY)
 
-for i in range(500):
-    angle = i * 137.5 * 180/pi
-    r = c * i**0.5
-    create_point(angle, 200, 200)
+create_spiral(200,200)
 
 root.mainloop()
